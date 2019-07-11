cc9fc832f6fa23682cdbeb62c6369899003470d3
chen.pcx <chen.pcx@alibaba-inc.com>
2019-07-11 16:06:41 +0800

    init

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # json-trans
-# json-trans
-# json-trans
-# json-trans
-# json-trans
-# json-trans
-# json-trans
+> Transform an object literal using JSONPath.
+> Forked from jsonpath-object-transform by dvdln [jsonpath-object-transform](https://github.com/dvdln/jsonpath-object-transform)
+> JSONPath is like XPath for JavaScript objects. To learn the syntax, read the documentation for the [JSONPath](https://www.npmjs.org/package/JSONPath) package on npm and the [original article](http://goessner.net/articles/JsonPath/) by Stefan Goessner.
+
+## Usage
+Kindly check test cases as for now.
+I'll add usage soon.
---
+++ example/example.js
@@ -0,0 +1,50 @@
+const transform = require('../src/index')
+
+const data = {
+  data: {
+    products: [{
+        title: 'led',
+        price: 100,
+        img: '//sc.alicdn.com/xxxxxxxx',
+        order: {
+          min: 200,
+          max: 5000
+        },
+        sku: [2, 4, 6]
+      },
+      {
+        title: 'crystal',
+        price: 500,
+        img: '//sc.alicdn.com/yyyyy',
+        order: {
+          min: 100,
+          max: 2000
+        },
+        sku: [1, 2, 3]
+      }
+    ],
+    supplier: {
+      name: 'alibaba',
+      location: 'China'
+    },
+    total: 2
+  },
+  success: true
+}
+
+const path = {
+  success: '$.success',
+  products: ['$.data.products', {
+    name: '@.title',
+    price: '@.price',
+    supplier: {
+      name: '$.data.supplier.name',
+      country: '$.data.supplier.location'
+    },
+    minOrder: '@.order.min',
+    maxOrder: '@.order.max',
+    sku: '@.sku'
+  }]
+}
+
+console.log(JSON.stringify(transform(data, path), null, 2));
